19ab225923deda0fb8b869576efd0e16684237ec
havenlybbqngrill <havenlybbqngrill@gmail.com>
2025-09-03 12:55:29 +0530

    Code updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,34 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Join from "./components/ConnectUs/Join";
 import Landing from "./pages/Landing";
 
 export default function Home() {
-  const [showPopup, setShowPopup] = useState(false);
-
-  useEffect(() => {
-    const popupHandled = localStorage.getItem("popupHandled");
-    if (!popupHandled) {
-      setShowPopup(true); // show Join popup only if not handled
-    }
-  }, []);
-
-  const handleClose = () => {
-    setShowPopup(false);
-    localStorage.setItem("popupHandled", "true"); // mark popup as handled
-  };
-
-  const handleJoinToday = () => {
-    setShowPopup(false);
-    localStorage.setItem("popupHandled", "true"); // also mark as handled
-  };
+  const [showPopup, setShowPopup] = useState(true);
 
   return (
     <div>
-      {showPopup ? (
-        <Join onClose={handleClose} onJoin={handleJoinToday} />
-      ) : (
-        <Landing />
-      )}
+      {showPopup && <Join onClose={() => setShowPopup(false)} />}
+      {!showPopup && <Landing />}
     </div>
   );
 }
