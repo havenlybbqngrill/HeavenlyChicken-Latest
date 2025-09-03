ea986745a140c29f09171863ccb72cfe3c3d7c71
havenlybbqngrill <havenlybbqngrill@gmail.com>
2025-09-03 12:33:38 +0530

    Update code

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,34 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Join from "./components/ConnectUs/Join";
-import Subscribe from "./components/ConnectUs/Subscribe";
 import Landing from "./pages/Landing";
 
 export default function Home() {
   const [showPopup, setShowPopup] = useState(true);
-  const [popupType, setPopupType] = useState("join"); // "join" or "subscribe"
-
-  const handleClose = () => {
-    setShowPopup(false);
-    localStorage.setItem("popupShown", "true");
-  };
-
-  useEffect(() => {
-    const alreadyShown = localStorage.getItem("popupShown");
-    if (alreadyShown) {
-      setShowPopup(false);
-    }
-  }, []);
 
   return (
-    <>
-      {showPopup && (
-        popupType === "join" ? (
-          <Join onClose={handleClose} />
-        ) : (
-          <Subscribe onClose={handleClose} />
-        )
-      )}
+    <div>
+      {showPopup && <Join onClose={() => setShowPopup(false)} />}
       {!showPopup && <Landing />}
-    </>
+    </div>
   );
 }
