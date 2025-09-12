673478aeb7c20abe348b4e3e5f071c712418c81b
havenlybbqngrill <havenlybbqngrill@gmail.com>
2025-09-12 15:25:27 +0530

    Error page update

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,12 @@
+import TopInfoBar from "../components/Landing/TopInfoBar";
+import Navbar from "../components/Landing/Navbar";
+import Contact from "../components/Landing/Contact";
+
+
 export default function NotFound() {
   return (
+     <TopInfoBar />
+      <Navbar />
     <div style={{ textAlign: "center", padding: "50px" }}>
       <h1>404 - Page Not Found</h1>
       <p>Oops! The page you are looking for does not exist.</p>
@@ -7,5 +14,6 @@ export default function NotFound() {
         Go back to Home
       </a>
     </div>
+     <Contact />
   );
 }
