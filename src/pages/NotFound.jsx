8dd535234f9aa6c76bc9c4e48b592b505b63c313
havenlybbqngrill <havenlybbqngrill@gmail.com>
2025-10-29 11:03:43 +0530

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,18 +2,19 @@ import TopInfoBar from "../components/Landing/TopInfoBar";
 import Navbar from "../components/Landing/Navbar";
 import Contact from "../components/Landing/Contact";
 
-
 export default function NotFound() {
   return (
-     <TopInfoBar />
+    <>
+      <TopInfoBar />
       <Navbar />
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>404 - Page Not Found</h1>
-      <p>Oops! The page you are looking for does not exist</p>
-      <a href="/" style={{ color: "blue", textDecoration: "underline" }}>
-        Go back to Home
-      </a>
-    </div>
-     <Contact />
+      <div style={{ textAlign: "center", padding: "50px" }}>
+        <h1>404 - Page Not Found</h1>
+        <p>Oops! The page you are looking for does not exist.</p>
+        <a href="/" style={{ color: "blue", textDecoration: "underline" }}>
+          Go back to Home
+        </a>
+      </div>
+      <Contact />
+    </>
   );
 }
