cfe21e0ebf1b38afdc40808550862a7ef359de5e
havenlybbqngrill <havenlybbqngrill@gmail.com>
2025-10-29 15:11:42 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </StrictMode>,
 )
